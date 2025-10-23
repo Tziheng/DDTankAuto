831baaa1ba8dec279e69a2e3e4f8575e2fb865f8
ziheng <ziheng@mail.ustc.edu.cn>
2025-10-23 21:30:45 +0800

    V2.1.1

--- agent/custom/action/flyanswer.py
+++ agent/custom/action/flyanswer.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from maa.agent.agent_server import AgentServer
 from maa.custom_action import CustomAction
 from maa.context import Context
-from utils import logger,time,RESOURCE_DIR
+from utils import logger,RESOURCE_DIR,time
 import os
 import json
 import pandas as pd
@@ -19,7 +20,7 @@ class FlyAnswer(CustomAction):
         logger.info("飞飞乐脚本开始运行！")
         args = {     
             "duration": 3600,
-            "n":3,
+            "n":1,
         }
         args.update(json.loads(argv.custom_action_param))
         duration = args["duration"]
@@ -33,31 +34,37 @@ class FlyAnswer(CustomAction):
         logger.debug(f"题库：{questionbank}")
         
         # 最长持续时间
-        cnt = 0
-        while cnt < duration:
-            # 截屏读取实时题目
-            img = context.tasker.controller.post_screencap().wait().get()
-            recodetail = context.run_recognition(
-                entry="OCR",
-                pipeline_override={"OCR":{"roi" : [250,180,750,300]}},
-                image=img,
-                )
-            all_results = recodetail.all_results
-            alltext = "".join([_.text for _ in all_results])
-            
-            # 匹配题目
-            best_match = process.extract(alltext, questionbank.keys(),limit=n)
-            
-            # 回答题目
-            output = ""
-            for i in range(n-1,-1,-1):
-                question = best_match[i][0]
-                answer = questionbank[question]    
-                score = best_match[i][1]
-                output += f"\n当前题目为：（置信度：{score:.0f}％）\n{question}\n"+f"该题答案：\n{answer}\n"
-            logger.info("\n"+"="*15+output+"="*15)
-            time.sleep(3)
-            cnt += 3
+        lastoutput = ""
+        start = datetime.now()
+        while (datetime.now() - start).total_seconds() < duration:
+            try:
+                # 截屏读取实时题目
+                img = context.tasker.controller.post_screencap().wait().get()
+                taskdetail = context.run_task(
+                    entry="OCR",
+                    pipeline_override={"OCR":{"roi" : [250,180,750,300]}},
+                    )
+                recodetail = taskdetail.nodes[0].recognition
+                all_results = recodetail.all_results
+                alltext = "".join([_.text for _ in all_results])
+                
+                # 匹配题目
+                best_match = process.extract(alltext, questionbank.keys(),limit=n)
+                
+                # 回答题目
+                output = "\n==============="
+                for i in range(n-1,-1,-1):
+                    question = best_match[i][0]
+                    answer = questionbank[question]    
+                    score = best_match[i][1]
+                    output += f"\n当前题目为：（置信度：{score:.0f}％）\n{question}\n"+f"该题答案：\n{answer}\n"
+                output += "===============\n"
+                if output != lastoutput:
+                    logger.info(output)
+                    lastoutput = output
+            except:
+                pass
+            time.sleep(1)
          
         logger.info("飞飞乐脚本运行结束！")
 
